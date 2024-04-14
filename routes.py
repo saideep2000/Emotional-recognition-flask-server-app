69b81d2009076f758938dfdad3803fd39d964f30
saideepsamineni <saminenisaideepchowdary@gmail.com>
2024-04-14 12:37:57 -0400

    added pkl files to s3 bucket

--- routes.py
+++ routes.py
@@ -1,14 +1,29 @@
 from flask import Blueprint, request, jsonify
 import pandas as pd
+import requests
+from io import BytesIO
 from .preprocessing import create_deepface_reps, preprocess_data
-from .svm import load_model, predict
-main = Blueprint('main', __name__)
+from .svm import predict
+import joblib
 
+main = Blueprint('main', __name__)
 emotions = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+
+model_url = 'https://assests-for-emotion-recognition-flask-app.s3.amazonaws.com/DeepFaceModel_SVM.pkl'
+
+def load_model_from_url(url):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    with BytesIO() as model_file:
+        for chunk in response.iter_content(chunk_size=8192):
+            model_file.write(chunk)
+        model_file.seek(0)
+        model = joblib.load(model_file)
+    return model
 
 @main.route("/getemotion-svm", methods=["POST"])
 def svm():
-    model = load_model('models/DeepFaceModel_SVM.pkl')
+    model = load_model_from_url(model_url)
     img_path = request.files['image']
     embeddings = create_deepface_reps(img_path)
     if embeddings is None:
